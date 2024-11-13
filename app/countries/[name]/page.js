13e5791a3146b3efa9eaa14d86db99b8d2b22477
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-11-13 20:22:37 +0800

    added countries data fetch logic

--- app/countries/[name]/page.js
+++ app/countries/[name]/page.js
@@ -1,3 +1,31 @@
-export default function Countries({ params }) {
-  return <h1>Country name: {params.name}</h1>;
+import Image from "next/image";
+
+// Helper function to get countries list
+async function getCountry(name) {
+  const res = await fetch(`https://restcountries.com/v3.1/name/${name}`, {
+    cache: "no-store",
+  });
+  return await res.json();
+}
+
+export default async function Countries({ params }) {
+  const countries = await getCountry(params.name);
+  return (
+    <>
+      <h1>Country name: {params.name}</h1>
+      <ul>
+        {countries.map((country, idx) => (
+          <li key={idx}>
+            <Image
+              src={country.flags.svg}
+              alt={country.flag + " flag"}
+              width={270}
+              height={150}
+            />
+            <span>{country.name.common}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,3 +1,33 @@
-export default function Login() {
-  return <h1>Dashboard</h1>;
+import { colors } from "@material-ui/core";
+import Image from "next/image";
+
+// Helper function to get countries list
+async function getCountries() {
+  const res = await fetch(`https://restcountries.com/v3.1/all`, {
+    cache: "no-store",
+  });
+  return await res.json();
+}
+
+export default async function Dashboard() {
+  const countries = await getCountries();
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <ul>
+        {countries.map((country, idx) => (
+          <li key={idx}>
+            <Image
+              src={country.flags.svg}
+              alt={country.flag + " flag"}
+              width={270}
+              height={150}
+            />
+            <span>{country.name.common}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
