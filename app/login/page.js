e6c6db8de568f313f4196ad8c274501daa37e3ce
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-11-13 21:43:01 +0800

    fix styling issue

--- app/login/page.js
+++ app/login/page.js
@@ -1,3 +1,24 @@
+import styles from "./Login.module.css";
+import Link from "next/link";
+
 export default function Login() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <div className={styles.card}>
+        <input
+          className="form-control mb-2"
+          type="text"
+          placeholder="Enter username"
+        />
+        <input
+          className="form-control mb-2"
+          type="password"
+          placeholder="Enter password"
+        />
+        <Link href="/dashboard" className="btn btn-light w-100">
+          Login
+        </Link>
+      </div>
+    </>
+  );
 }
